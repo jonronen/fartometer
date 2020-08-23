463227441d3fb20d9bb7060b3eb55ba0474d3e23
Jon Ronen-Drori <jon_ronen@yahoo.com>
2020-08-23 20:22:41 +0300

    Remove the log file from the run script

--- run-fartometer.sh
+++ run-fartometer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /home/pi/sdk-cpp-workspace/fartometer
-/usr/bin/python sense_main.py --endpoint "a5r16h71o6euo-ats.iot.us-west-2.amazonaws.com" --cert /home/pi/AWS_Certs/9edb4854f4-certificate.pem.crt --key /home/pi/AWS_Certs/9edb4854f4-private.pem.key --root-ca /home/pi/AWS_Certs/AmazonRootCA1.pem --client-id "arn:aws:iot:us-west-2:580778568985:thing/HomePi3A" > /home/pi/`date +"fartometer.%F.%T.log"` 2>&1
+/usr/bin/python sense_main.py --endpoint "a5r16h71o6euo-ats.iot.us-west-2.amazonaws.com" --cert /home/pi/AWS_Certs/9edb4854f4-certificate.pem.crt --key /home/pi/AWS_Certs/9edb4854f4-private.pem.key --root-ca /home/pi/AWS_Certs/AmazonRootCA1.pem --client-id "arn:aws:iot:us-west-2:580778568985:thing/HomePi3A"
 
