664741145a888318de45bd8cf1c179ec8f8f8520
Jon Ronen-Drori <jon_ronen@yahoo.com>
2020-08-29 12:32:57 +0100

    Run the service in a while loop

--- run-fartometer.sh
+++ run-fartometer.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 cd /home/pi/sdk-cpp-workspace/fartometer
-/usr/bin/python sense_main.py --endpoint "a5r16h71o6euo-ats.iot.us-west-2.amazonaws.com" --cert /home/pi/AWS_Certs/9edb4854f4-certificate.pem.crt --key /home/pi/AWS_Certs/9edb4854f4-private.pem.key --root-ca /home/pi/AWS_Certs/AmazonRootCA1.pem --client-id "arn:aws:iot:us-west-2:580778568985:thing/HomePi3A"
+while true
+do
+	/usr/bin/python sense_main.py --endpoint "a5r16h71o6euo-ats.iot.us-west-2.amazonaws.com" --cert "/home/pi/AWS_Certs/6de2893c32-certificate.pem.crt" --key "/home/pi/AWS_Certs/6de2893c32-private.pem.key" --client-id "arn:aws:iot:us-west-2:580778568985:thing/HomePiZero" --root-ca /home/pi/AWS_Certs/AmazonRootCA1.pem --sensor ccs811
+	sleep 60
+done
 
